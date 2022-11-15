62ae4557106fc113be5587647b7ecb88a1d81022
myat-thandarlynn91 <piggythandar.myat@gmail.com>
2022-11-15 19:32:45 +0900

    #1

---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>My Todos</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <h1>Todos</h1>
+        <span class="purge">Purge</span>
+      </header>
+
+    <form>
+      <input type="text" name="title" placeholder="Type new todo.">
+    </form>
+
+    <ul>
+        <li>
+        <input type="checkbox">
+        <span class="delete">x</span>
+      </li>
+    </ul>
+  </main>
+  <script src="js/main.js"></script>
+</body>
+</html>
